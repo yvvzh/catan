91d4d8ed5b0841de223605607f5a574ec6d18b5f
yvvzh <135761301+yvvzh@users.noreply.github.com>
2024-02-16 15:50:12 +0100

    need to fix citySpotsAttributes

--- src/boardInit.js
+++ src/boardInit.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector(".grid-container");
+const matrix = document.querySelector(".interactive-grid");
 
 const tiles = [
     {
@@ -59,7 +60,7 @@ const tiles = [
     },
 ];
 
-const tokens = ["5", "2", "6", "3", "8", "10", "9", "12", "11", "4", "8", "10", "9", "4", "5", "6", "3", "11"];
+const tokens = ["5", "2", "6", "10", "9", "4", "3", "8", "11", "5", "8", "4", "3", "6", "10", "11", "12", "9"];
 
 const trades = [
     {
@@ -158,9 +159,48 @@ let usedClayTrade = 0;
 let usedRockTrade = 0;
 let usedSheepTrade = 0;
 
+const patternCSA = [
+    [0, 3], // port 1
+    [1, 5], // port 2
+    [0, 3, 4, 7, 8, 12], // tile 1
+    [1, 4, 5, 8, 9, 13], // tile 2
+    [2, 5, 6, 9, 10, 14], // tile 3
+    [6, 10], // port 3
+    [11, 16], // port 4
+    [7, 11, 12, 16, 17, 22], // tile 4
+    [8, 12, 13, 17, 18, 23], // tile 5
+    [9, 13, 14, 18, 19, 24], // tile 6
+    [10, 14, 15, 19, 20, 25], // tile 7
+    [16, 21, 22, 27, 28, 33], // tile 8
+    [17, 22, 23, 28, 29, 34], // tile 9
+    [18, 23, 24, 29, 30, 35], // tile 10
+    [19, 24, 25, 30, 31, 36], // tile 11
+    [20, 25, 26, 31, 32, 37], // tile 12
+    [26, 32], // port 5
+    [27, 23], // port 6
+    [28, 33, 34, 38, 39, 43], // tile 13
+    [29, 34, 35, 39, 40, 44], // tile 14
+    [30, 35, 36, 40, 41, 45], // tile 15
+    [31, 36, 37, 41, 42, 46], // tile 16
+    [39, 43, 44, 47, 48, 51], // tile 17
+    [40, 44, 45, 48, 49, 52], // tile 18
+    [41, 45, 46, 49, 50, 53], // tile 19
+    [46, 50], // port 7
+    [47, 51], // port 8
+    [49, 52], // port 9
+];
+let rawCSA = [];
+
+let citySpotsAttributes = [];
+let roadSpotsAttributes = [];
+let usedSpots = [];
+
 boardInit();
+console.log(citySpotsAttributes);
 
 function boardInit() {
+    gridInit();
+    let patternCSAindex = 0;
     let tokensIndex = 0;
     for (let i = 0; i < pattern.length; i++) {
         const tile = document.createElement("div");
@@ -178,6 +218,20 @@ function boardInit() {
                 number.classList.add("number");
                 number.textContent = tokens[tokensIndex];
                 token.appendChild(number);
+                if (assetLink.includes("wheat")) {
+                    manageRawCSA("ressource", patternCSA[patternCSAindex], "wheat");
+                } else if (assetLink.includes("clay")) {
+                    manageRawCSA("ressource", patternCSA[patternCSAindex], "clay");
+                } else if (assetLink.includes("rock")) {
+                    manageRawCSA("ressource", patternCSA[patternCSAindex], "rock");
+                } else if (assetLink.includes("wood")) {
+                    manageRawCSA("ressource", patternCSA[patternCSAindex], "wood");
+                } else if (assetLink.includes("sheep")) {
+                    manageRawCSA("ressource", patternCSA[patternCSAindex], "sheep");
+                }
+
+                manageRawCSA("number", patternCSA[patternCSAindex], tokens[tokensIndex]);
+                patternCSAindex++;
 
                 if (tokens[tokensIndex] === "6" || tokens[tokensIndex] === "8") {
                     token.classList.add("great-odds");
@@ -198,6 +252,8 @@ function boardInit() {
                 const name = document.createElement("div");
                 name.textContent = trades[tradeIndex].name.toUpperCase();
                 token.appendChild(name);
+                manageRawCSA("trade", patternCSA[patternCSAindex], trades[tradeIndex].name);
+                patternCSAindex++;
             }
             const ratio = document.createElement("div");
             ratio.textContent = `${trades[tradeIndex].ratio} : 1`;
@@ -206,6 +262,136 @@ function boardInit() {
         }
         grid.appendChild(tile);
     }
+    manageCitySpotAttributes();
+}
+
+function gridInit() {
+    const tradeSpotPattern = [0, 1, 3, 5, 6, 10, 11, 16, 26, 27, 32, 33, 46, 47, 49, 50, 51, 52];
+    for (let i = 0; i < 126; i++) {
+        const spot = document.createElement("div");
+        spot.id = `spot${i}`;
+        if (i < 54) {
+            spot.classList.add("spot", "citySpot");
+            if (tradeSpotPattern.includes(i)) {
+                spot.classList.add("tradeSpot");
+            }
+        } else {
+            spot.classList.add("spot", "roadSpot");
+            const newRoadSpot = {
+                id: spot.id,
+                owner: "none",
+                built: false,
+            };
+            roadSpotsAttributes.push(newRoadSpot);
+        }
+
+        matrix.appendChild(spot);
+    }
+}
+
+function manageRawCSA(type, spots, value) {
+    switch (type) {
+        case "trade":
+            spots.forEach((spot) => {
+                const spotIndex = rawCSA.findIndex(function (obj) {
+                    return obj["id"] === spot;
+                });
+                if (spotIndex !== -1) {
+                    rawCSA[spotIndex].trade = value;
+                } else {
+                    const newRawCSA = {
+                        id: spot,
+                        numbers: [],
+                        ressources: [0, 0, 0, 0, 0],
+                        trade: value,
+                    };
+                    rawCSA.push(newRawCSA);
+                }
+            });
+            break;
+
+        case "ressource":
+            let indexToItterate;
+
+            switch (value) {
+                case "wheat":
+                    indexToItterate = 0;
+                    break;
+                case "clay":
+                    indexToItterate = 1;
+                    break;
+                case "rock":
+                    indexToItterate = 2;
+                    break;
+                case "wood":
+                    indexToItterate = 3;
+                    break;
+                case "sheep":
+                    indexToItterate = 4;
+                    break;
+                default:
+                    console.log("switch case for ressources in manageRawCSA() FAILED");
+            }
+
+            spots.forEach((spot) => {
+                const spotIndex = rawCSA.findIndex(function (obj) {
+                    return obj["id"] === spot;
+                });
+                if (spotIndex !== -1) {
+                    rawCSA[spotIndex].ressources[indexToItterate]++;
+                } else {
+                    const newRawCSA = {
+                        id: spot,
+                        numbers: [],
+                        ressources: [0, 0, 0, 0, 0],
+                        trade: "none",
+                    };
+                    newRawCSA.ressources[indexToItterate]++;
+                    rawCSA.push(newRawCSA);
+                }
+            });
+            break;
+
+        case "number":
+            spots.forEach((spot) => {
+                const spotIndex = rawCSA.findIndex(function (obj) {
+                    return obj["id"] === spot;
+                });
+                if (spotIndex !== -1) {
+                    rawCSA[spotIndex].numbers.push(value);
+                } else {
+                    const newRawCSA = {
+                        id: spot,
+                        numbers: [],
+                        ressources: [0, 0, 0, 0, 0],
+                        trade: "none",
+                    };
+                    newRawCSA.numbers.push(value);
+                    rawCSA.push(newRawCSA);
+                }
+            });
+            break;
+        default:
+            console.log("manageRawCSA() switch case FAILED");
+    }
+}
+
+function manageCitySpotAttributes() {
+    rawCSA.forEach((e) => {
+        const newCSA = {
+            id: `spot${e.id}`, // spot id
+            numbers: e.numbers, // numbers associated
+            wheat: e.ressources[0], // qty
+            clay: e.ressources[1], // qty
+            rock: e.ressources[2], // qty
+            wood: e.ressources[3], // qty
+            sheep: e.ressources[4], // qty
+            trade: e.trade, // name of the trade
+            owner: "none",
+            building: "none",
+        };
+        citySpotsAttributes.push(newCSA);
+    });
 }
 
 function assetSelector(i) {
